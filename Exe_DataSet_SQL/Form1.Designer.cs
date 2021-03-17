d00757ed3d415608773f51be4d5594dd9878ee8b
Netanel Aknin <netanelos152@gmail.com>
2021-03-17 17:14:57 +0200

    Add project files.

---
+++ Exe_DataSet_SQL/Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿
+namespace Exe_DataSet_SQL
+{
+    partial class update_DB_btn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.InputClub = new System.Windows.Forms.TextBox();
+            this.InputCoach = new System.Windows.Forms.TextBox();
+            this.InputWin = new System.Windows.Forms.TextBox();
+            this.InputDraw = new System.Windows.Forms.TextBox();
+            this.InputLose = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnInsertTeam = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.InputDel = new System.Windows.Forms.TextBox();
+            this.del_team_btn = new System.Windows.Forms.Button();
+            this.update_team_btn = new System.Windows.Forms.Button();
+            this.update_db = new System.Windows.Forms.Button();
+            this.search_btn = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.InputId = new System.Windows.Forms.TextBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.select_btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.Location = new System.Drawing.Point(27, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(383, 24);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "DataSet and DataAdapter Soccer Teams";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(496, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(743, 210);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label2.Location = new System.Drawing.Point(25, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Insert";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(27, 122);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Coach Name:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(27, 97);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Club Name:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(27, 148);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(37, 13);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Wins :";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(26, 174);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(43, 13);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Draws :";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(27, 206);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(41, 13);
+            this.label10.TabIndex = 10;
+            this.label10.Text = "Loses :";
+            // 
+            // InputClub
+            // 
+            this.InputClub.Location = new System.Drawing.Point(129, 94);
+            this.InputClub.Name = "InputClub";
+            this.InputClub.Size = new System.Drawing.Size(100, 20);
+            this.InputClub.TabIndex = 11;
+            // 
+            // InputCoach
+            // 
+            this.InputCoach.Location = new System.Drawing.Point(129, 120);
+            this.InputCoach.Name = "InputCoach";
+            this.InputCoach.Size = new System.Drawing.Size(100, 20);
+            this.InputCoach.TabIndex = 12;
+            // 
+            // InputWin
+            // 
+            this.InputWin.Location = new System.Drawing.Point(129, 146);
+            this.InputWin.Name = "InputWin";
+            this.InputWin.Size = new System.Drawing.Size(100, 20);
+            this.InputWin.TabIndex = 13;
+            // 
+            // InputDraw
+            // 
+            this.InputDraw.Location = new System.Drawing.Point(129, 172);
+            this.InputDraw.Name = "InputDraw";
+            this.InputDraw.Size = new System.Drawing.Size(100, 20);
+            this.InputDraw.TabIndex = 14;
+            // 
+            // InputLose
+            // 
+            this.InputLose.Location = new System.Drawing.Point(129, 202);
+            this.InputLose.Name = "InputLose";
+            this.InputLose.Size = new System.Drawing.Size(100, 20);
+            this.InputLose.TabIndex = 15;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label11.Location = new System.Drawing.Point(113, 61);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(68, 20);
+            this.label11.TabIndex = 16;
+            this.label11.Text = "Update";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label3.Location = new System.Drawing.Point(25, 502);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 20);
+            this.label3.TabIndex = 17;
+            this.label3.Text = "Delete";
+            // 
+            // btnInsertTeam
+            // 
+            this.btnInsertTeam.Location = new System.Drawing.Point(75, 247);
+            this.btnInsertTeam.Name = "btnInsertTeam";
+            this.btnInsertTeam.Size = new System.Drawing.Size(75, 23);
+            this.btnInsertTeam.TabIndex = 18;
+            this.btnInsertTeam.Text = "Insert Team";
+            this.btnInsertTeam.UseVisualStyleBackColor = true;
+            this.btnInsertTeam.Click += new System.EventHandler(this.btnInsertTeam_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(25, 542);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(52, 13);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "Id Team :";
+            // 
+            // InputDel
+            // 
+            this.InputDel.Location = new System.Drawing.Point(129, 539);
+            this.InputDel.Name = "InputDel";
+            this.InputDel.Size = new System.Drawing.Size(100, 20);
+            this.InputDel.TabIndex = 20;
+            // 
+            // del_team_btn
+            // 
+            this.del_team_btn.Location = new System.Drawing.Point(254, 537);
+            this.del_team_btn.Name = "del_team_btn";
+            this.del_team_btn.Size = new System.Drawing.Size(93, 23);
+            this.del_team_btn.TabIndex = 21;
+            this.del_team_btn.Text = "Delete Team";
+            this.del_team_btn.UseVisualStyleBackColor = true;
+            this.del_team_btn.Click += new System.EventHandler(this.del_team_btn_Click);
+            // 
+            // update_team_btn
+            // 
+            this.update_team_btn.Location = new System.Drawing.Point(185, 247);
+            this.update_team_btn.Name = "update_team_btn";
+            this.update_team_btn.Size = new System.Drawing.Size(96, 23);
+            this.update_team_btn.TabIndex = 22;
+            this.update_team_btn.Text = "Update Team";
+            this.update_team_btn.UseVisualStyleBackColor = true;
+            this.update_team_btn.Click += new System.EventHandler(this.update_team_btn_Click);
+            // 
+            // update_db
+            // 
+            this.update_db.Location = new System.Drawing.Point(1119, 228);
+            this.update_db.Name = "update_db";
+            this.update_db.Size = new System.Drawing.Size(120, 42);
+            this.update_db.TabIndex = 23;
+            this.update_db.Text = "Update DataBase";
+            this.update_db.UseVisualStyleBackColor = true;
+            this.update_db.Click += new System.EventHandler(this.update_db_Click);
+            // 
+            // search_btn
+            // 
+            this.search_btn.Location = new System.Drawing.Point(206, 359);
+            this.search_btn.Name = "search_btn";
+            this.search_btn.Size = new System.Drawing.Size(75, 23);
+            this.search_btn.TabIndex = 24;
+            this.search_btn.Text = "Serach";
+            this.search_btn.UseVisualStyleBackColor = true;
+            this.search_btn.Click += new System.EventHandler(this.search_btn_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label8.Location = new System.Drawing.Point(24, 324);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(201, 20);
+            this.label8.TabIndex = 25;
+            this.label8.Text = "Search Teams by points";
+            // 
+            // InputId
+            // 
+            this.InputId.Location = new System.Drawing.Point(100, 360);
+            this.InputId.Name = "InputId";
+            this.InputId.Size = new System.Drawing.Size(90, 20);
+            this.InputId.TabIndex = 26;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(22, 363);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(73, 13);
+            this.label12.TabIndex = 27;
+            this.label12.Text = "Insert points  :";
+            // 
+            // select_btn
+            // 
+            this.select_btn.Location = new System.Drawing.Point(496, 228);
+            this.select_btn.Name = "select_btn";
+            this.select_btn.Size = new System.Drawing.Size(84, 42);
+            this.select_btn.TabIndex = 28;
+            this.select_btn.Text = "Refersh Table";
+            this.select_btn.UseVisualStyleBackColor = true;
+            this.select_btn.Click += new System.EventHandler(this.select_btn_Click);
+            // 
+            // update_DB_btn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1251, 657);
+            this.Controls.Add(this.select_btn);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.InputId);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.search_btn);
+            this.Controls.Add(this.update_db);
+            this.Controls.Add(this.update_team_btn);
+            this.Controls.Add(this.del_team_btn);
+            this.Controls.Add(this.InputDel);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.btnInsertTeam);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.InputLose);
+            this.Controls.Add(this.InputDraw);
+            this.Controls.Add(this.InputWin);
+            this.Controls.Add(this.InputCoach);
+            this.Controls.Add(this.InputClub);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Name = "update_DB_btn";
+            this.Text = "DataSet And DataAdapter Sql Program";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox InputClub;
+        private System.Windows.Forms.TextBox InputCoach;
+        private System.Windows.Forms.TextBox InputWin;
+        private System.Windows.Forms.TextBox InputDraw;
+        private System.Windows.Forms.TextBox InputLose;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnInsertTeam;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox InputDel;
+        private System.Windows.Forms.Button del_team_btn;
+        private System.Windows.Forms.Button update_team_btn;
+        private System.Windows.Forms.Button update_db;
+        private System.Windows.Forms.Button search_btn;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox InputId;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Button select_btn;
+    }
+}
+
---
+++ Exe_DataSet_SQL/Form1.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exe_DataSet_SQL
+{
+    public partial class update_DB_btn : Form
+    {
+        public update_DB_btn()
+        {
+            InitializeComponent();
+        }
+
+        string strCon = @"Data Source=DESKTOP-FTT4EST\SQLEXPRESS;Initial Catalog=SoccerDB;Integrated Security=True";
+        SqlConnection con;
+        SqlDataAdapter adptr;
+        DataSet dsTeams;
+        DataTable dtTeams;
+        static int count = 1;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dsTeams = new DataSet();
+
+            con = new SqlConnection(strCon);
+            adptr = new SqlDataAdapter(
+                " SELECT * " +
+                " FROM Teams ", con);
+            ShowDataFromDB();
+        }
+        private void select_btn_Click(object sender, EventArgs e)
+        {
+            ShowDataFromDB();
+        }
+
+        private void ShowDataFromDB()
+        {
+            dsTeams.Clear();
+            adptr.Fill(dsTeams, "TeamsTemp");
+            dtTeams = dsTeams.Tables["TeamsTemp"];
+
+            dataGridView1.DataSource = dtTeams;
+        }
+
+        private void btnInsertTeam_Click(object sender, EventArgs e)
+        {
+            int win = int.Parse(InputWin.Text);
+            int draw = int.Parse(InputDraw.Text);
+            DataRow dr = dtTeams.NewRow();
+            dr["id_team"] = count++;
+            dr["club_name"] = InputClub.Text;
+            dr["coach_name"] = InputCoach.Text;
+            dr["wins"] = InputWin.Text;
+            dr["draws"] = InputDraw.Text;
+            dr["loses"] = InputLose.Text;
+            dr["points"] = win * 3 + draw;
+            dtTeams.Rows.Add(dr);
+            MessageBox.Show("Row Added", "Add", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void del_team_btn_Click(object sender, EventArgs e)
+        {
+            foreach (DataRow row in dtTeams.Rows)
+            {
+                if (row.RowState != DataRowState.Deleted && row["id_team"].ToString() == InputDel.Text)
+                {
+                    row.Delete();
+                    MessageBox.Show("Row Deleted", "Delete", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+
+
+        }
+
+        private void update_team_btn_Click(object sender, EventArgs e)
+        {
+            foreach (DataRow row in dtTeams.Rows)
+            {
+                if (row.RowState != DataRowState.Deleted && row["club_name"].ToString() == InputClub.Text)
+                {
+                    if (InputClub.Text == "" || InputCoach.Text == "" || InputWin.Text == "" || InputDraw.Text == "" || InputLose.Text == "")
+                    {
+                        MessageBox.Show("Please Fill all the fields", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        return;
+                    }
+                    row["id_team"] = count;
+                    row["club_name"] = InputClub.Text;
+                    row["coach_name"] = InputCoach.Text;
+                    row["wins"] = InputWin.Text;
+                    row["draws"] = InputDraw.Text;
+                    row["loses"] = InputLose.Text;
+                    row["points"] = (int.Parse(InputWin.Text) * 3) + int.Parse(InputDraw.Text);
+                    MessageBox.Show("Team Updated", "Update", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+        private void update_db_Click(object sender, EventArgs e)
+        {
+            new SqlCommandBuilder(adptr);
+            adptr.Update(dtTeams);
+            MessageBox.Show("DataBase Updated", "Update DB", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+        }
+
+        private void search_btn_Click(object sender, EventArgs e)
+        {
+            SqlCommand comm = new SqlCommand("SearchTeamsTable", con);
+            comm.CommandType = CommandType.StoredProcedure;
+
+            SqlParameter parID = new SqlParameter("Points",InputId.Text);
+            parID.Direction = ParameterDirection.Input;
+            comm.Parameters.Add(parID);
+
+            SqlDataAdapter aptr2 = new SqlDataAdapter(comm);
+            DataSet ds2 = new DataSet();
+            aptr2.Fill(ds2, "T1");
+
+            dataGridView1.DataSource = ds2.Tables["T1"];
+        }
+
+    }
+}
+
